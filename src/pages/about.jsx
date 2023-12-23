86cbae3cd3b851f16b269f5f0fcea9a9841df581
David <78326757+daols1@users.noreply.github.com>
2023-12-23 01:40:18 +0100

    First Commit- About page done, Not responsive yet

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,18 @@
 import Layout from "../components/layout"
+import Aboutmain from "../components/ui/Aboutmain"
+import Goals from "../components/ui/Goals"
+import MissionVision from "../components/ui/MissionVision"
+import Quote from "../components/ui/Quote"
+import Team from "../components/ui/Team"
 
 export default function AboutPage() {
-  return <Layout>AboutPage</Layout>
+  return (
+    <Layout>
+      <Aboutmain />
+      <MissionVision />
+      <Team />
+      <Quote />
+      <Goals />
+    </Layout>
+  )
 }
