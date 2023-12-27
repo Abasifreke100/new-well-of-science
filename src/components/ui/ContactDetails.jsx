5dc322d1ca832377b05eda27acb18b287505b92d
David <78326757+daols1@users.noreply.github.com>
2023-12-27 18:15:00 +0100

    Contact page done with overlay.

--- src/components/ui/ContactDetails.jsx
+++ src/components/ui/ContactDetails.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import SectionPadding from './SectionPadding'
 import { Mail, MapPin, PhoneCallIcon } from 'lucide-react'
-import { Button } from './button'
+import ContactForm from './ContactForm'
 
 const ContactDetails = () => {
 
-    const [overlay, setOverlay] = useState(false)
 
 
 
@@ -46,24 +45,8 @@ const ContactDetails = () => {
             </div>
             <div>
                 {/* Form */}
-                <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                    <form action="#" className="space-y-8">
-                        <div>
-                            <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 ">Full Name</label>
-                            <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 " placeholder="John Doe" required />
-                        </div>
-                        <div>
-                            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email Address</label>
-                            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 k:placeholder-gray-400 " placeholder="name@wellness.com" required />
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Message</label>
-                            <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Leave a Message..."></textarea>
-                        </div>
-                        <Button asChild className={"hidden md:flex"}>
-                            <p>Submit</p>
-                        </Button>
-                    </form>
+                <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md bg-[url(/images/lines.png)] ">
+                    <ContactForm />
                 </div>
             </div>
         </div>
