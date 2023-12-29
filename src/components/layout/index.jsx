b353b25acee25e264d66206891734a71bc1fb151
xeuxdev <tochukwujohn24@gmail.com>
2023-12-29 10:02:11 +0100

    fix scroll restoration

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,12 +1,16 @@
 import Header from "./header"
 import Footer from "./footer"
 import MotionWrapper from "../motion-wrapper"
+import ScrollToTop from "../scroll-to-top"
 
 export default function Layout({ children }) {
   return (
     <MotionWrapper>
       <Header />
-      <main className="relative py-20">{children}</main>
+      <main className="relative py-20">
+        <ScrollToTop />
+        {children}
+      </main>
       <Footer />
     </MotionWrapper>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import HomePage from "./pages/home"
+import AboutPage from "./pages/about"
+import PortfolioPage from "./pages/portfolio"
+import PartnershipPage from "./pages/partnership"
+import ContactPage from "./pages/contact"
+import PartnerShipFormPage from "./pages/partnership-form"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "*",
+    element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/portfolio",
+    element: <PortfolioPage />,
+  },
+  {
+    path: "/partnership",
+    element: <PartnershipPage />,
+  },
+  {
+    path: "partnership-form",
+    element: <PartnerShipFormPage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
