5ba027ae2c691a6d753ad532d6aef6c9efa4f38d
xeuxdev <tochukwujohn24@gmail.com>
2023-12-30 23:00:20 +0100

    contact form hooked up

--- src/components/partnership/partners-form.jsx
+++ src/components/partnership/partners-form.jsx
@@ -35,9 +35,8 @@ export default function PartnersForm() {
   } = useForm({
     resolver: zodResolver(FormSchema),
   })
-  const onSubmit = (data) => {
+  const onSubmit = () => {
     setStatus("pending")
-    console.log(data)
 
     emailjs
       .sendForm(
--- src/components/ui/ContactForm.jsx
+++ src/components/ui/ContactForm.jsx
@@ -1,83 +1,103 @@
-import React, { useState } from 'react'
-import { Button } from './button'
-import { useForm } from 'react-hook-form'
-import OverlayMessage from './OverlayMessage'
-
-
+import emailjs from "@emailjs/browser"
+import { useRef, useState } from "react"
+import { useForm } from "react-hook-form"
+import OverlayMessage from "./OverlayMessage"
+import { Button } from "./button"
 
 const ContactForm = () => {
+  const [overlay, setOverlay] = useState(false)
+  const { register, handleSubmit, formState } = useForm()
 
-    const [overlay, setOverlay] = useState(false)
-    const {register, handleSubmit, formState} = useForm()
-    const onSubmit = (data) => {
-        console.log(data)
-        setOverlay(true)
-    }
-    const formError = formState.errors
+  const formRef = useRef()
+
+  const onSubmit = (data) => {
+    console.log(data)
+    emailjs
+      .sendForm(
+        "service_r1blhvf",
+        "template_6utxzk7",
+        formRef.current,
+        "PtKad8KQjae9K4wCT"
+      )
+      .then(
+        () => {
+          setOverlay(true)
+        },
+        () => {
+          setOverlay(false)
+        }
+      )
+  }
+  const formError = formState.errors
 
   return (
-    <form 
-    action="#" 
-    className="space-y-8  "
-    onSubmit={handleSubmit(onSubmit)}
+    <form
+      ref={formRef}
+      className="space-y-8 "
+      onSubmit={handleSubmit(onSubmit)}
     >
-        <div>
-            <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 ">Full Name</label>
-            <input 
-            type="text" 
-            id="subject" 
-            className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 " 
-            placeholder="John Doe" 
-            required
-            {
-                ...register("fullName", { required: true })
-            }
-             />
-        </div>
-        <div>
-            <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Email Address</label>
-            <input 
-            type="email" 
-            id="email" 
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  "
-            placeholder="name@wellness.com" 
-            required 
-            {
-                ...register("email", {
-                    pattern: {
-                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                        message:'Please put in a valid email'
-                    }
-                })
-            }
-            />
-            {formError.body && formError.body.type === "pattern" && <span className='errors'>Please put in a valid email</span>}
-        </div>
-        <div className="sm:col-span-2">
-            <label for="message" className="block mb-2 text-sm font-medium text-gray-900">Message</label>
-            <textarea 
-            id="message" 
-            rows="6" 
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " 
-            placeholder="Leave a Message..."
-            {
-                ...register("message", { required: true })
-            }
-            ></textarea>
-        </div>
-        <Button asChild className={"flex"}>
-            {/* <p>Submit</p> */}
-            <input type='submit'/>
-        </Button>
-        {
-            overlay ? 
-                <OverlayMessage />
-                // You can setup as dynamic component later
-            : 
-
-            null
-        }
-        
+      <div>
+        <label
+          htmlFor="subject"
+          className="block mb-2 text-sm font-medium text-gray-900 "
+        >
+          Full Name
+        </label>
+        <input
+          type="text"
+          id="subject"
+          name="full_name"
+          className="block w-full p-3 text-sm text-gray-900 border border-gray-300 rounded-lg shadow-sm bg-gray-50 focus:ring-primary-500 focus:border-primary-500 "
+          placeholder="John Doe"
+          required
+          {...register("fullName", { required: true })}
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          Email Address
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  "
+          placeholder="name@wellness.com"
+          required
+          {...register("email", {
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Please put in a valid email",
+            },
+          })}
+        />
+        {formError.body && formError.body.type === "pattern" && (
+          <span className="errors">Please put in a valid email</span>
+        )}
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="message"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          Message
+        </label>
+        <textarea
+          id="message"
+          rows="6"
+          name="message"
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 "
+          placeholder="Leave a Message..."
+          {...register("message", { required: true })}
+        ></textarea>
+      </div>
+      <Button asChild className={"flex"}>
+        <input type="submit" />
+      </Button>
+      {overlay ? <OverlayMessage /> : null}
     </form>
   )
 }
