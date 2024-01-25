8e213daae00e3170c376e6e7748d16cadbe3bbc8
JC-Coder <josephchimezie2003@gmail.com>
2024-01-25 08:29:20 +0100

    update

--- src/components/home/blog-section.jsx
+++ src/components/home/blog-section.jsx
@@ -9,14 +9,10 @@ export default function BlogSection() {
 
   return (
     <section className="relative py-10 bg-white">
-      <header className="pt-4 pb-10 mx-auto mb-10 space-y-6 text-center px-7 md:mb-16">
+      <header className="pt-4 pb-10 mx-auto mb-10 space-y-6 text-center px-7 md:mb-12">
         <h5 className="text-dark_text font-semibold font-gen_sans text-2xl md:text-[32px] leading-[130%] lg:text-[40px] ">
           Explore our Blog
         </h5>
-
-        <p className="text-sm font-inter lg:text-base">
-          Here are some of the blogs we&apos;ve published
-        </p>
       </header>
 
       <div className="grid grid-cols-1 gap-10 px-4 md:grid-cols-2 msl:grid-cols-3 md:px-10 place-items-center md:place-items-start msl:place-items-center">
